c0ca64b4f8be8ee47ffbda5f276217303215b82e
Diego Fernandes <eaa24daf@students.eaaa.dk>
2025-02-11 09:09:02 +0100

    second commit

--- src/views/DetailPage.jsx
+++ src/views/DetailPage.jsx
@@ -5,8 +5,10 @@ export default function DetailPage(){
     const eventdetail = [
         {uid: "1", time:"Coldplay will play at 10 am"},
         {uid: "2", time:"Queen will play at 12 noon without Freddy"},
-        {uid: "3", time:"Billie Eilish will play at 2pm"},
-        {uid: "4", time:"lady Gaga will play at 4 pm"},
+        {uid: "3", time:"Van Morrison will play at 2pm"},
+        {uid: "4", time:"Toto will play at 3pm"},
+        {uid: "5", time:"Billie Eilish will play at 2pm"},
+        {uid: "6", time:"lady Gaga will play at 4 pm"},
     ]
 
     const {eventId} = useParams();
--- src/views/StagePage.jsx
+++ src/views/StagePage.jsx
@@ -1,25 +1,50 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 export default function StagePage(){
 
     const stageevents = [
-        {stageid : "1", eventtext:"Coldplay", uid:"1"},
-        {stageid : "1", eventtext:"Queen", uid:"2"},
-        {stageid : "2", eventtext:"Billie Eilish", uid:"3"},
-        {stageid : "2", eventtext:"Lady Gaga", uid: "4"} 
+        {stageid : "1", eventtext:"Coldplay", uid:"1", category:"Pop"},
+        {stageid : "1", eventtext:"Queen", uid:"2",category:"Rock"},
+        {stageid : "1", eventtext:"Van Morrison", uid:"3",category:"Folk"},
+        {stageid : "1", eventtext:"Toto", uid:"4",category:"Pop"},
+        {stageid : "2", eventtext:"Billie Eilish", uid:"5",category:"Pop"},
+        {stageid : "2", eventtext:"Lady Gaga", uid: "6",category:"Pop"} 
     ];
 
 
     const{ stageId } = useParams(); //Get stageId from Link
+    const [searchTerm, setSearchTerm]= useState('');
+    const [selectedCategory, setSelectedCategory] = useState('');
+
+
+
 
     //Get events that matches stageId and id from link
     //Result: an array of stageevents called "events"
-    const events = stageevents.filter((event) => stageId === event.stageid);
+    const events = stageevents.filter((event,) => stageId === event.stageid);
+    const filteredEvents = events.filter(event => {
+        const matchesSearch = event.eventtext.toLowerCase().includes(searchTerm.toLowerCase());
+        const matchesCategory = selectedCategory === 'all' || event.category === selectedCategory;
+        return matchesSearch && matchesCategory;
+    })
 
     return (
         <div>
+            Search by artist:
+            <input type= "text" id="search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search for artist"/>
+
+            Filter by category:
+            <select id="category" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+                <option value="all">All</option>
+                <option value="Pop">Pop</option>
+                <option value="Rock">Rock</option>
+                <option value="Folk">Folk</option>
+
+            </select>
             <h2>List of Events on stage {stageId}</h2>
-            {events.map((event)=>(
+            {filteredEvents.map((event)=>(
                 <div key={event.id}>
                     <Link to={`/musicalfestival/detailpage/${event.uid}`}>{event.eventtext}</Link>
                 </div>
